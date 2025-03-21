15b28a739ed1e747a300a73d0c6f1043121f0b03
John Fitzgerald Nocum <johnfitzgerald0310@gmail.com>
2025-03-21 14:46:47 +0000

    Completed 03-hashing

--- src/hash_table.c
+++ src/hash_table.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <string.h>
+#include <math.h>
 
 #include "hash_table.h"
 
@@ -35,4 +36,14 @@ void ht_del_hash_table(ht_hash_table* ht) {
 	}
 	free(ht->pairs);
 	free(ht);
+}
+
+static int ht_hash(const char* s, const int a, const int m) {
+	long hash = 0;
+	const int len_s = strlen(s);
+	for (int i = 0; i < len_s; i++) {
+		hash += (long)pow(a, len_s - (i+1)) * s[i];
+		hash %= m;
+	}
+	return hash;
 }
